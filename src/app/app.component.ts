973a75f0f047d8a87df62410ed04dfa297b028f0
Paul <paul@beambient.co.uk>
2021-06-15 14:50:21 +0100

    commit before new course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
+    console.log(`
+    About to generate a password with the following: 
+    Includes letters: ${this.includeLetters}
+    Includes numbers: ${this.includeNumbers}
+    Includes symbols: ${this.includeSymbols}`);
+
     this.password = 'MY PASSWORD!!!';
   }
 }
